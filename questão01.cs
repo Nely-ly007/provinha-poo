376f15bdfb69a06ecfe53708738df7e403e81469
Nely Victória Oliveira da Silva <o.nely@escolar.ifrn.edu.br>
2024-08-18 21:05:52 -0300

    questão1
    Escreva um programa em C# que declare variáveis dos tipos int, float, string e bool. Atribua valores a essas variáveis e imprima-os no console.

---
+++ questão01.cs
@@ -0,0 +1,8 @@
+int a = 2;
+Console.WriteLine($"o valor int é {a}");
+float b = 5.9f;
+Console.WriteLine($"o valor float é {b}");
+string c = "casa";
+Console.WriteLine($"a palavra string é {c}");
+bool d = 7>9;
+Console.WriteLine($"o valor bool é {d}");
